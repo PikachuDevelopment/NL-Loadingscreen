948763fe9095f1f4ae31b5fb9be2c440c4fee0bf
PikachuDevelopment <141827527+PikachuDevelopment@users.noreply.github.com>
2023-08-19 23:56:30 +0200

    Main files Added

---
+++ NL-loadingscreen/__resource.lua
@@ -0,0 +1,20 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+version '1.5.0'
+
+loadscreen 'html/index.html'
+
+files {
+	'html/index.html',
+	'html/css/bootstrap.min.css',
+	'html/css/custom.css',
+	'html/css/morphext.css',
+	'html/js/jquery.min.js',
+	'html/js/bootstrap.min.js',
+	'html/js/popper.min.js',
+	'html/js/app.js',
+	'html/js/morphext.min.js',
+	'html/img/logo.png',
+	'html/img/back.png',
+	'html/song.mp3'
+}
